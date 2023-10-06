405111a0eb4896b9565188e8ed5537ed05491e6a
romain-abdouh <romain.abdouh@laplateforme.io>
2023-10-06 18:19:04 +0200

    Dernier ajout job

--- JOB09/accessrights.sh
+++ JOB09/accessrights.sh
@@ -1,24 +1,21 @@
+#!/bin/bash
 sed '1d' Shell_Userlist.csv > temp_file.csv
+while IFS=',' read -r id prenom nom mdp role; do 
+    nom_utilisateur="${prenom} ${nom}"
 
-
-while IFS=',' read -r id prenom nom mot_de_passe role; do
-	role=${role:0:(${#role}-1)} 
-	nom_utilisateur=$(echo "$prenom $nom")
-
-  if [ -d "/home/$nom_utilisateur" ]; then
-	if [ "$role" = "Admin" ]; then
-	sudo usermod -aG sudo "$nom_utilisateur"
-	else
-	sudo usermod -G users "$nom_utilisateur"
-	fi
-    echo "L'utilisateur $nom_utilisateur existe déjà. Ignoré."
-  else
-   sudo useradd "$nom_utilisateur" --badname --uid "$id" --create-home
-   echo "$nom_utilisateur:$mdp" | sudo chpasswd
-	if [ "$role" = "Admin" ]; then
-	sudo usermod -aG sudo "$nom_utilisateur"
-   	fi
-  fi
+    if id "$nom_utilisateur" &>/dev/null; then
+        if [ "$role" = "Admin" ]; then
+            sudo usermod -aG sudo "$nom_utilisateur"
+        else
+            sudo usermod -G users "$nom_utilisateur"
+        fi
+        echo "L'utilisateur $nom_utilisateur existe déjà. Ignoré."
+    else
+        sudo useradd -m -c "$role" -s /bin/bash -p  "$mdp" -u "$id" "$nom_utilisateur" --badname
+        if [ "$role" = "Admin" ]; then
+            sudo usermod -aG sudo "$nom_utilisateur"
+        fi
+    fi
 done < temp_file.csv
 
 rm temp_file.csv
